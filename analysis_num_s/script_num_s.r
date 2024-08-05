b53220949aa3d8f6bea8260beb4103eaab879937
Lexi <lexi@192.168.1.13>
2024-08-05 19:09:33 -0300

    num_s histogram, ogive code and png results

--- analysis_num_s/script_num_s.r
+++ analysis_num_s/script_num_s.r
@@ -8,7 +8,7 @@ View(udemy_data)
 summary_num_s <- summary(udemy_data$num_subscribers)
 
 # Given the large number of observations, use a class division within this variable
-clases_num_s <- seq(0,40000, by = 4000)
+clases_num_s <- seq(from = 0,to = 40000, by = 4000)
 clases_num_s_x <- cut(udemy_data$num_subscribers, breaks = clases_num_s)
 FrecGrossPM_num_s  <- data.frame(table (clases_num_s_x))
 FrecGrossPM_num_s
@@ -23,6 +23,24 @@ FrecPor_num_s <- FrecRel_num_s*100
 DisFrec_num_s <- cbind (FrecAbs_num_s, FrecRel_num_s, FrecPor_num_s)
 
 # Graphics and more
-# Create Bar plot for absolute frequency 
-barplot(FrecAbs_num_s)
+
+# Bar graphic
+# Create bar graphic for absolute frequency 
+# Define colors object to use in graphics
+my_colors <- c("lavender", "mistyrose", "lightcyan", "lightblue", "cornsilk")
+# Create Bar plot for absolute frequency, apply colors object and
+barplot(FrecAbs_num_s, col = my_colors, beside = TRUE, main = "Bar chart of num_subscribers", xlab = "Class division", ylab = "amount of Udemy courses")
+
+# Histogram
+# For some graphs, it is necessary to download agricolae library
+install.packages("agricolae")
+library(agricolae)
+# Create an object that contains a histogram
+hist_num_s <- hist(udemy_data$num_subscribers, col = my_colors, main = "Histogram of num_subscribers", xlab = "Number of subscribers", ylab = "Number of courses")
+
+# Ogive 
+# Based on hist_num_s object, create an ogive
+ogive.freq(hist_num_s, ,type="b", col = "purple", xlab = "Number of subscribers", ylab = "Absolute Frequency")
+
+
 
