c6cc16e97ce4a5cdb63e31e5047d87edf8bd756c
Lexi <lexi@192.168.1.13>
2024-08-10 23:49:18 -0300

    added png Scatter diagram relating num_subscriber and is_paid

--- analysis_correlation/script_correlation.r
+++ analysis_correlation/script_correlation.r
@@ -35,34 +35,63 @@ library(ggplot2)
 # Scatter Diagram: rating & num_subscribers
 ggplot(udemy_data, aes(x=udemy_data$rating, 
                        y=udemy_data$num_subscribers, 
-                       group=clases_rating_x)) + geom_point(aes(color=clases_rating_x)) + scale_color_manual(values=c(my_colors)) + ggtitle("Scatter of rating & number of subscribers") + xlab("Rating") + ylab("Numer of Subscribers") + theme_bw()
+                       group=clases_rating_x)) + geom_point(aes(color=
+                                                clases_rating_x)) + scale_color_manual(values=
+                                                c(wes_palette(n=5, name="Darjeeling1"))) + ggtitle("Scatter of rating & number of subscribers") + xlab("
+                                                Rating") + ylab("Numer of Subscribers") + theme_bw()
 # Another way of the same scatter diagram could be
 plot(udemy_data$rating, udemy_data$num_subscribers, main = 'Correlation', 
      ylab = 'Number of subscribers',xlab = 'Rating', 
-     col=clases_rating_x) 
+     col=wes_palette(n=5, name="Darjeeling1")[factor(clases_rating_x)]) 
 # Next line adds a smal square with color explanation
 legend(x = "topleft", 
        legend = c("(0, 100]", "(100, 200]", "(200, 300]", 
                   "(300, 400]", "(400,500]"), 
-       fill = c("lavender", "mistyrose", "lightcyan", "lightblue", "cornsilk"), 
-       title = "Ranting") # FIX COLORS
+       fill = wes_palette(n=5, name="Darjeeling1"), 
+       title = "Ranting") 
 
 # Scatter Diagram: rating, num_subscribers & is_paid
 # Create Scatter Diagram
 ggplot(udemy_data, aes(x=udemy_data$rating, 
                        y=udemy_data$num_subscribers, 
-                       group=udemy_data$is_paid)) + geom_point(aes(color=udemy_data$is_paid)) + scale_color_manual(values=c("mistyrose", 
-                      "lightblue")) + ggtitle("Scatter of is_paid") +
+                       group=udemy_data$is_paid)) + geom_point(aes(color=udemy_data$is_paid)) + scale_color_manual(values=c(wes_palette(n=2, 
+                        name="Darjeeling1"))) + ggtitle("Scatter of is_paid") +
                         xlab("Rating") + ylab("Numer of Subscribers") + theme_bw() 
 # Another way of the same scatter diagram could be
-plot(x = udemy_data$rating, y = udemy_data$num_subscribers, col= udemy_data$is_paid) # FIX COLORS
+plot(x = udemy_data$rating, y = udemy_data$num_subscribers, 
+     col= wes_palette(n=2, name="Darjeeling1")) # FIX COLORS
 # Next line adds a smal square with color explanation
 legend(x = "topleft", 
        legend = c("True", "False"), 
-       fill = c("mistyrose", "lightblue"), 
+       fill = c(wes_palette(n=2, name="Darjeeling1")), 
        title = "Status is_paid")
 
-
-
-
+# Bar graphic
+# This has issue: ✖ Fix the following mappings: `y`.
+ggplot(udemy_data, aes(fill=udemy_data$is_paid, 
+                 y= FrecGrossPM__num_s,
+                 x=udemy_data$num_subscribers,
+                 group=clases_num_s_x)) + scale_color_manual(values=
+                 c(wes_palette(n=2, name="Darjeeling1"))) + ggtitle("Scatter of number of subscribers 
+                 & is_paid") + xlab("
+                 Number of subscribers") + ylab("Numer of courses") + theme_bw(
+                 ) + geom_bar(position="dodge2", stat="identity", 
+                fill = wes_palette(n=2, name="Darjeeling1"), 
+                colour = "black")
+# This one solves it, but bars are too together and I can not modify this
+# which is why I am trying to use ggplot to solve this
+barplot(FrecAbs_num_s, 
+        col=wes_palette(n=2, name="Darjeeling1"), 
+        font.axis=2, 
+        beside=T, 
+        ylim = c(0,14000), 
+        main="Bar chart of is_pais absolute frequency", 
+        xlab="Number of subscribers", 
+        ylab="Amount of courses", 
+        font.lab=2, 
+        names.arg = c("(0, 4000]", "(4000, 8000]", "(8000, 12000]", "(12000, 16000]", "(16000,20000]", "(20000, 24000]", "(24000, 28000]", "(28000, 32000]", "(32000, 36000]", 
+                      "(36000, 40000]"))
+legend(x = "topright", 
+       legend = c("True", "False"), 
+       fill = wes_palette(n=2, name="Darjeeling1"), title = "Status is_paid")
 
