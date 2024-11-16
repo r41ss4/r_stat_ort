f3cc317db935f151c8f760d690438610cbf5d2a6
Lexi <lexi@Lexis-MacBook-Pro.local>
2024-11-16 11:33:56 +0100

    Reorder script to first download libraries and fix error with location

--- analysis_rating/script_rating.r
+++ analysis_rating/script_rating.r
@@ -1,7 +1,21 @@
-# Download necessary library to read file 
+# Install necessary libraries
+if (!require(readr)) install.packages("readr")
+if (!require(wesanderson)) install.packages("wesanderson")
+if (!require(agricolae)) install.packages("agricolae")
+if (!require(plotly)) install.packages("plotly")
+if (!require(gt)) install.packages("gt")
+if (!require(gtsummary)) install.packages("gtsummary")
+
+# Load libraries
 library(readr)
+library(wesanderson)
+library(agricolae)
+library(plotly)
+library(gt)
+library(gtsummary)
+
 # Import and read udemy_data.csv
-udemy_data <- read_csv("udemy_data.csv")
+udemy_data <- read_csv("/Users/lexi/Documents/data_repos/r_stat_ort/udemy_data.csv")
 View(udemy_data)
 
 # Observe  the minimum, maximum, quartiles, mean, median and NA data
@@ -10,46 +24,39 @@ summary_rating <- summary(udemy_data$rating)
 # Given the large number of observations, use a class division within this variable
 clases_rating <- seq(from = 0,to = 500, by = 100)
 clases_rating_x <- cut(udemy_data$rating, breaks = clases_rating)
-FrecGrossPM_rating  <- data.frame(table (clases_rating_x))
-FrecGrossPM_rating
+frecGrossPM_rating  <- data.frame(table (clases_rating_x))
+frecGrossPM_rating
 
 # Frequencies
 # Create the object for the absolute frequency
-FrecAbs_rating <- table(clases_rating_x)
+frecAbs_rating <- table(clases_rating_x)
 # Create the object for relative frequency
-FrecRel_rating <- prop.table(FrecAbs_rating)
+frecRel_rating <- prop.table(frecAbs_rating)
 # Calculate percentage frequency
-FrecPor_rating <- FrecRel_rating*100
+frecPor_rating <- frecRel_rating*100
 # Based on the absolute, relative and percentage frequency, calculate the frequency distribution
-DisFrec_rating <- cbind (FrecAbs_rating, FrecRel_rating, FrecPor_rating)
+disFrec_rating <- cbind (frecAbs_rating, frecRel_rating, frecPor_rating)
 
 
 # Graphics and more
 
 # Bar graphic
-# Create bar graphic for absolute frequency 
-# Define colors object to use in graphics
-install.packages('wesanderson')
-library('wesanderson')
 # Create an object with ranges
 rang_label_rating = c("(0, 100]", "(100, 200]", "(200, 300]", 
                       "(300, 400]", "(400,500]")
-# Create Bar plot for absolute frequency, apply colors object and
-bar_rating <- barplot(FrecAbs_rating, 
+# Create Bar plot for absolute frequency
+bar_rating <- barplot(frecAbs_rating, 
               col=wes_palette(n=5, name="Darjeeling1"), 
               beside = TRUE, main = "Bar chart of rating", 
               xlab = "Rating", ylab = "Amount of Udemy courses", 
               names.arg = rang_label_rating) 
-# Adds a small square with color explanation
+# Add legend
 legend(x = "topleft", 
        legend = c(rang_label_rating), 
        fill = wes_palette(n=5, name="Darjeeling1"), 
        title = "Ranges", xpd=TRUE, cex=0.8, inset=c(+.03, -.05)) 
 
 # Histogram
-# For some graphs, it is necessary to download agricolae library
-install.packages("agricolae")
-library(agricolae)
 # Create an object that contains a histogram
 hist_rating <- hist(udemy_data$rating, col=wes_palette(n=10, name="Darjeeling1", 
                type = "continuous"), ylim = c(0,6000),
@@ -67,15 +74,11 @@ ogive_rating <- ogive.freq(hist_rating,type="b", col=wes_palette(n=1, name="Darj
                 xlab = "Rating", ylab = "Absolute Frequency")
 
 # Pie chart
-# Install selected package to make piecharts (plotly)
-install.packages("plotly")
-# Call needed libraries
-library(plotly)
-#Determinar FrecAbs_num_s as data.frame
-FrecAbs_rating_frame = data.frame(FrecAbs_rating)
+# Determinar frecAbs_num_s as data.frame
+frecAbs_rating_frame = data.frame(frecAbs_rating)
 # Create pie chart for absolute frequency
-pie_rating = plot_ly(FrecAbs_rating_frame, labels = rang_label_rating, 
-                    values=FrecPor_rating, type = 'pie', 
+pie_rating = plot_ly(frecAbs_rating_frame, labels = rang_label_rating, 
+                    values=frecPor_rating, type = 'pie', 
                     marker=list(colors=c(wes_palette(n=5, name="Darjeeling1")))) %>% layout(
                     title="Pie Chart of rating absolute frequency")
 pie_rating 
@@ -131,11 +134,6 @@ atip_rating <- boxplot(udemy_data$rating,
  
 
 # Tables
-# Install necessary libraries 
-install.packages("gt")
-library(gt)
-library(gtsummary)
-
 # Summary Table
 # Needed element names for table
 summary_rating_names <- names(summary_rating)
@@ -146,6 +144,7 @@ summary_rating_tabl <- summary_rating_frame%>%
   gt() %>% tab_header(title 
   = "Summary num_subscribers")%>% opt_row_striping() %>% 
   opt_table_lines("all")
+summary_rating_tabl
 
 # Deciles table
 # Needed element names for table
@@ -157,6 +156,7 @@ decils_rating_tabl <- decils_rating_frame%>%
   gt() %>% tab_header(title 
   = "Deciles percentage and tendencies")%>% opt_row_striping() %>% 
   opt_table_lines("all")
+decils_rating_tabl
 
 # Quintiles table
 # Needed element names for table
@@ -168,6 +168,7 @@ quint_rating_tabl <- quint_rating_frame%>%
   gt() %>% tab_header(title 
   = "Quintiles percentage and tendencies")%>% opt_row_striping() %>% 
   opt_table_lines("all")
+quint_rating_tabl
 
 # Quartiles table
 # Needed element names for table
@@ -179,3 +180,4 @@ quarts_rating_tabl <- quarts_rating_frame%>%
   gt() %>% tab_header(title 
   = "Quartiles percentage and tendencies")%>% opt_row_striping() %>% 
   opt_table_lines("all") 
+quarts_rating_tabl
