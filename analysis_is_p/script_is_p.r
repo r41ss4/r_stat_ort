b9226097f3c4bf9ca02dbfba2592f5c993a8e7e5
Lexi <lexi@Lexis-MacBook-Pro.local>
2024-11-16 10:44:39 +0100

    Reorder script to first download libraries and fix error with location

--- analysis_is_p/script_is_p.r
+++ analysis_is_p/script_is_p.r
@@ -1,7 +1,18 @@
-# Download necessary library to read file 
+# Install necessary library
+# Install selected package to make piecharts (plotly)
+install.packages("plotly")
+# Install necessary libraries 
+install.packages("gt")
+
+# Load necessary library
 library(readr)
+library(plotly)
+library(gt)
+library(gtsummary)
+library(wesanderson)
+
 # Import and read udemy_data.csv
-udemy_data <- read_csv("udemy_data.csv")
+udemy_data <- read_csv("/Users/lexi/Documents/data_repos/r_stat_ort/udemy_data.csv")
 View(udemy_data)
 
 # Summarized amount of FALSE and TRUE observations
@@ -9,20 +20,19 @@ summary_is_p <- summary(udemy_data$is_paid)
 
 # Frequencies
 # Create the object for the absolute frequency
-FrecAbs_is_p <- table(udemy_data$is_paid)
+frecAbs_is_p <- table(udemy_data$is_paid)
 # Create the object for relative frequency
-FrecRel_is_p <- prop.table(FrecAbs_is_p)
+frecRel_is_p <- prop.table(frecAbs_is_p)
 # Calculate percentage frequency
-FrecPor_is_p <- FrecRel_is_p*100
+frecPor_is_p <- frecRel_is_p*100
 # Based on the absolute, relative and percentage frequency, calculate the frequency distribution
-DistFrec_is_p <- cbind(FrecAbs_is_p,FrecRel_is_p,FrecPor_is_p)
+distfrec_is_p <- cbind(frecAbs_is_p,frecRel_is_p,frecPor_is_p)
 
 
 # Graphics and more
-
 # Bar graphic
 # Create bar graphic for absolute frequency 
-bar_s_paid <- barplot(FrecAbs_is_p, ylim = c(0,14000),
+bar_s_paid <- barplot(frecAbs_is_p, ylim = c(0,14000),
               xlim= c(0,5), main="Bar chart of is_pais absolute frequency", 
               xlab="is_paid", ylab="Freq. Absoluta",col=wes_palette(n=2, name="Darjeeling1"))
 # Adds a small square with color explanation
@@ -32,35 +42,26 @@ legend(x = "topright",
        title = "Status is_paid")
 
 # Pie chart
-# Install selected package to make piecharts (plotly)
-install.packages("plotly")
-# Call needed libraries
-library(plotly)
 # Create an object with ranges
 label_s_p = c("False", "True")
 #Determinar FrecAbs_num_s as data.frame
-FrecAbs_is_p_frame = data.frame(FrecAbs_is_p)
+frecAbs_is_p_frame = data.frame(frecAbs_is_p)
 # Create pie chart for absolute frequency
-pie_is_p = plot_ly(FrecAbs_is_p_frame, labels = label_s_p, 
-                     values=FrecPor_is_p, type = 'pie', 
+pie_is_p = plot_ly(frecAbs_is_p_frame, labels = label_s_p, 
+                     values=frecPor_is_p, type = 'pie', 
                      marker=list(colors=c(wes_palette(n=2, name="Darjeeling1")))) %>% layout(
                      title="Pie Chart of is_paid absolute frequency")
 pie_is_p
 
 # Table
-# Install necessary libraries 
-install.packages("gt")
-library(gt)
-library(gtsummary)
-
 # Summary Table
 # Needed element names for table
 summary_is_p_names <- names(summary_is_p)
 # Transform summary into a data frame
 summary_is_p_frame <- data.frame(Statistics = summary_is_p_names, Value = as.numeric(summary_is_p))
 # Create table with gt() 
-summary_is_p_tabl <- summary_is_p_frame%>% 
+summary_is_p_tabl <- summary_is_p_frame %>% 
   gt() %>% tab_header(title 
-  = "Summary is_paid")%>% opt_row_striping() %>% 
+  = "Summary is_paid") %>% opt_row_striping() %>% 
   opt_table_lines("all")
-
+summary_is_p_tabl
