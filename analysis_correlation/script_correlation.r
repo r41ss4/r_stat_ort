f2f53c100ee88fe10838aa20046edf7b5b86782d
Lexi <lexi@Lexis-MacBook-Pro.local>
2024-11-16 12:15:34 +0100

    Change reading path to github raw url

--- analysis_correlation/script_correlation.r
+++ analysis_correlation/script_correlation.r
@@ -1,3 +1,27 @@
+# Download necessary libraries
+if (!require(readr)) install.packages("readr")
+if (!require(ggplot2)) install.packages("ggplot2")
+if (!require(gmodels)) install.packages("gmodels")
+if (!require(wesanderson)) install.packages("wesanderson")
+if (!require(modelsummary)) install.packages("modelsummary")
+if (!require(webshot2)) install.packages("webshot2")
+if (!require(gt)) install.packages("gt")
+if (!require(gtsummary)) install.packages("gtsummary")
+
+# Load libraries
+library(readr)
+library(ggplot2)
+library(gmodels)
+library(wesanderson)
+library(modelsummary)
+library(webshot2)
+library(gt)
+library(gtsummary)
+
+# Import and read udemy_data.csv from repository
+udemy_data <- read_csv("https://raw.githubusercontent.com/r41ss4/r_stat_ort/main/udemy_data.csv")
+View(udemy_data)
+
 # Correlation analysis measurements
 # Covariances
 cov_num_rating = cov(udemy_data$num_subscribers, udemy_data$rating)
@@ -15,13 +39,9 @@ reg_num_paid = lm(udemy_data$num_subscribers~udemy_data$is_paid)
 reg_paid_rating = lm(udemy_data$is_paid~udemy_data$rating)
 
 # Verification with r2 (correlation square)
-corr_num_rating^2
+corr_num_rating^2 
 
 # Cross Tabulation
-# Install necessary package
-install.packages("gmodels")
-# Call library
-library(gmodels)
 # Create the object for cross tabulation 
 crosstab_num_paid <- CrossTable(clases_num_s_x, udemy_data$is_paid, 
                      dnn = c("Classes num_subscribers", "Status is_paid"), 
@@ -38,9 +58,6 @@ clases_rating_x
 # Number of Subscribers
 clases_num_s <- seq(from = 0,to = 40000, by = 4000)
 clases_num_s_x
-# Install necessary libraries 
-install.packages("ggplot2")
-library(ggplot2)
 # Create an object with ranges for rating
 rang_label_rating = c("(0, 100]", "(100, 200]", "(200, 300]", 
                       "(300, 400]", "(400,500]")
@@ -61,7 +78,7 @@ scat_corr_r_n <- ggplot(udemy_data, aes(x=udemy_data$rating,
 scat_plot_corr_r_n <- plot(udemy_data$rating, udemy_data$num_subscribers, main = 'Correlation', 
      ylab = 'Number of subscribers',xlab = 'Rating', 
      col=wes_palette(n=5, name="Darjeeling1")[factor(clases_rating_x)]) 
-# Adds a small square with color explanation
+# Add legend
 legend(x = "topleft", 
        legend = c(rang_label_rating), 
        fill = wes_palette(n=5, name="Darjeeling1"), 
@@ -75,10 +92,11 @@ scat_corr_r_n_p <- ggplot(udemy_data, aes(x=udemy_data$rating,
                    name="Darjeeling1"))) + ggtitle("Scatter of is_paid") +
                    xlab("Rating") + ylab("Numer of Subscribers") + theme_bw() 
 # Another way of the same scatter diagram could be
-scat_corr_plot_r_n_p <- plot(x = udemy_data$rating, y = udemy_data$num_subscribers, 
+scat_corr_plot_r_n_p <- plot(x = udemy_data$rating, y = udemy_data$num_subscribers,
                         ylab = 'Rating',xlab = 'Number of subscribers', 
-                        col= wes_palette(n=2, name="Darjeeling1")) 
-# Adds a small square with color explanation
+                        col= wes_palette(n=2, name="Darjeeling1"), 
+                        main = 'Scatter Plot of Rating vs. Number of Subscribers') 
+# Add legend
 legend(x = "topleft", 
        legend = c("True", "False"), 
        fill = c(wes_palette(n=2, name="Darjeeling1")), 
@@ -89,7 +107,7 @@ legend(x = "topleft",
 table_is_p <- table(udemy_data$is_paid, clases_num_s_x)
 # Create group barplot
 bar_group_n_p <- barplot(table_is_p, xlab="Number of subscribers", 
-                 main="Grouped Barplot: num_subscribers & is_paid",beside=TRUE, 
+                 main="Num_subscribers & is_paid",beside=TRUE, 
                  ylim = c(0,15000), 
                  names.arg = rang_label_num_s, 
                  col = c(wes_palette(n=2, name="Darjeeling1")), 
@@ -100,17 +118,7 @@ legend(x = "topright",
        fill = wes_palette(n=2, name="Darjeeling1"), 
        title = "Status is_paid")
 
-
 # Tables
-# Necessary libraries
-install.packages("modelsummary")
-library(modelsummary)
-install.packages("webshot2")
-library(webshot2)
-install.packages("gt")
-library(gt)
-library(gtsummary)
-
 # Covariances table
 # Needed element names for table
 summary_num_s_names <- names(summary_num_s)
@@ -124,6 +132,7 @@ cov_tabl <- cov_frame%>%
   gt() %>% tab_header(title 
            = "Covariance analysis") %>% 
            opt_row_striping() %>% opt_table_lines("all")
+cov_tabl
 
 # Correlation table
 # Transform into a data frame
@@ -136,6 +145,7 @@ corr_tabl <- corr_frame%>%
   gt() %>% tab_header(title 
            = "Covariance analysis") %>% 
            opt_row_striping() %>% opt_table_lines("all")
+corr_tabl
 
 # Regressions tables
 modelsummary(reg_num_rating, output = "reg_num_rating.png")
